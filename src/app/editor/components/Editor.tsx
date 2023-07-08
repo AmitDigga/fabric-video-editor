c1fce58e9b99f46049d9c372ca4488017487d0e1
Amit Digga <diggaamitoo7@gamil.com>
2023-07-08 09:43:16 +0530

    Grid Styled

--- src/app/editor/components/Editor.tsx
+++ src/app/editor/components/Editor.tsx
@@ -26,59 +26,68 @@ export const Editor = () => {
     });
   }, []);
   return (
-    <div>
-      <div>
-        <div className="flex flex-row">
-          {videos.map((video, index) => {
-            return (
-              <div key={index}>
-                <button
-                  onClick={() => {
-                    if (!canvas) return;
-                    const videoElement = getHtmlVideoElement(
-                      document.getElementById(`video-${index}`)
-                    );
-                    const videoObject = new fabric.Image(videoElement, {
-                      // backgroundColor: "red",
-                      left: 20,
-                      top: 20,
-                      objectCaching: false,
-                      stroke: "black",
-                      strokeWidth: 1,
-                      selectable: true,
-                      // scaleX: 0.5,
-                      // scaleY: 0.5,
-                    });
-                    // videoObject.scaleToHeight(300);
-                    // videoObject.scaleToWidth(300);
-                    canvas.add(videoObject);
-                  }}
-                >
-                  Add
-                </button>
-                <button
-                  onClick={() => {
-                    setVideos(videos.filter((_, i) => i !== index));
-                  }}
-                >
-                  Remove
-                </button>
-                <video
-                  src={video}
-                  controls
-                  height={200}
-                  width={200}
-                  id={`video-${index}`}
-                  // className="h-[200px] w-[200px]"
-                ></video>
-              </div>
-            );
-          })}
-          <input type="file" onChange={handleFileChange} />
-        </div>
+    <div className="grid grid-rows-[60%_40%] grid-cols-[60px_200px_1fr] h-[100%]">
+      <div className="tile row-span-2 bg-slate-400">Menu</div>
+      <div className="row-span-2 flex flex-col bg-slate-200">
+        {videos.map((video, index) => {
+          return (
+            <div key={index} className="rounded bg-slate-800 m-4 flex flex-col">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-[100px]"
+                onClick={() => {
+                  if (!canvas) return;
+                  const videoElement = getHtmlVideoElement(
+                    document.getElementById(`video-${index}`)
+                  );
+                  const videoObject = new fabric.Image(videoElement, {
+                    // backgroundColor: "red",
+                    left: 20,
+                    top: 20,
+                    objectCaching: false,
+                    stroke: "black",
+                    strokeWidth: 1,
+                    selectable: true,
+                  });
+                  canvas.add(videoObject);
+                }}
+              >
+                Add
+              </button>
+              <video
+                src={video}
+                height={200}
+                width={200}
+                id={`video-${index}`}
+              ></video>
+            </div>
+          );
+        })}
+        <label
+          htmlFor="fileInput"
+          className="flex flex-col justify-center items-center bg-gray-500 rounded-lg cursor-pointer m-4  h-[150px] text-white"
+        >
+          <input id="fileInput" type="file"  className="hidden" onChange={handleFileChange} />
+          Add Video
+          <svg
+            className="h-6 w-6 text-white mx-auto"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            />
+          </svg>
+        </label>
       </div>
-      <div>Hello</div>
-      <canvas id="canvas" className="h-[400px] w-[400px]" />
+      <canvas
+        id="canvas"
+        className="h-[400px] w-[400px] row col-start-3 start-1"
+      />
+      <div className="bg-slate-500 col-start-3 row-start-2">time line</div>
     </div>
   );
 };
