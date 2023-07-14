fd70c619728f3b78495664efaaf0b6d6cba31341
Amit Digga <diggaamitoo7@gamil.com>
2023-07-14 14:40:23 +0530

    Update Color picker predefined colors

--- src/components/panels/FillPanel.tsx
+++ src/components/panels/FillPanel.tsx
@@ -4,6 +4,29 @@ import { StoreContext } from "@/store";
 import { observer } from "mobx-react";
 import { BlockPicker } from "react-color";
 
+const professionalVideoColors = [
+  "#000000", // Black
+  "#FFFFFF", // White
+  "#404040", // Dark Gray
+  "#808080", // Gray
+  "#C0C0C0", // Silver
+  "#E0E0E0", // Light Gray
+  "#003366", // Dark Blue
+  "#336699", // Medium Blue
+  "#6699CC", // Blue
+  "#99CCFF", // Light Blue
+  "#990000", // Dark Red
+  "#CC3333", // Red
+  "#FF6666", // Light Red
+  "#663300", // Dark Brown
+  "#996633", // Brown
+  "#CC9966", // Light Brown
+  "#006600", // Dark Green
+  "#339933", // Green
+  "#66CC99", // Light Green
+  "#FFFF00", // Yellow
+];
+
 export const FillPanel = observer(() => {
   const store = React.useContext(StoreContext);
   // Color Picker
@@ -14,6 +37,7 @@ export const FillPanel = observer(() => {
       </div>
       <div className="flex items-center justify-center">
         <BlockPicker
+          colors={professionalVideoColors}
           color={store.backgroundColor}
           onChangeComplete={(color: any) => {
             console.log(color);
