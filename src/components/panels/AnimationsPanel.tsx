ca8a0dc6a07cb618cb2cc2f8202537cd4184ae65
Amit Digga <diggaamitoo7@gamil.com>
2023-07-14 12:07:11 +0530

    Add WIP badge in animation panel

--- src/components/panels/AnimationsPanel.tsx
+++ src/components/panels/AnimationsPanel.tsx
@@ -9,6 +9,9 @@ export const AnimationsPanel = observer(() => {
     <>
       <div className="text-sm px-[16px] pt-[16px] pb-[8px] font-semibold">
         Animations
+        <span className="ml-2 bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">
+          WIP
+        </span>
       </div>
       {store.animations.map((animation) => {
         return <div key={animation.id}>{animation.targetId}</div>;
